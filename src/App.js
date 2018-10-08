75f04efbc9af64f5aee1ab40f3458bf4100d5ccc
arielbk <buchwald.kearney@gmail.com>
2018-10-08 19:40:36 +0200

    context API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment  } from 'react';
 import './App.css';
 import { Toggle } from 'Utilities';
 import { Modal } from 'Elements';
+import User from './User';
+import { UserContext } from './UserContext';
+
+class UserProvider extends Component {
+  state = {
+    id: '123',
+    name: 'ariel',
+    email: 'ariel@arielbk.com'
+  }
+
+  render() {
+    return (
+      <UserContext.Provider
+        value={{
+          user: this.state
+        }}
+      >
+        {this.props.children}
+      </UserContext.Provider>
+    )}
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Toggle>
-          {({on, toggle}) => (
-            <Fragment>
-              <button onClick={toggle}>Login</button>
-              <Modal on={on} toggle={toggle}>
-                <h1>Still!</h1>
-              </Modal>
-            </Fragment>
-          )}
-        </Toggle>
-
-      </div>
+      <UserProvider>
+        <div className="App">
+          <User />
+          <Toggle>
+            {({on, toggle}) => (
+              <Fragment>
+                <button onClick={toggle}>Login</button>
+                <Modal on={on} toggle={toggle}>
+                  <h1>Still!</h1>
+                </Modal>
+              </Fragment>
+            )}
+          </Toggle>
+        </div>
+      </UserProvider>
     );
   }
 }
